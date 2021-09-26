d51c72280cdb2e57c484a3c187bda3f42384daf0
Andres Javier Castillo <ajcastillov11@gmail.com>
2021-09-26 16:08:42 -0500

    260920211608

--- src/app/components/dashboard/usuarios/usuarios.component.ts
+++ src/app/components/dashboard/usuarios/usuarios.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/interfaces/usuario';
+
+
+const ELEMENT_DATA: Usuario[] = [
+  { usuario:'acastillo',nombre:'Andres',apellido:'Castillo',sexo:'M'},
+  { usuario:'acastillo',nombre:'Andres',apellido:'Castillo',sexo:'M'},
+ 
+];
 
 @Component({
   selector: 'app-usuarios',
@@ -6,6 +14,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuarios.component.css']
 })
 export class UsuariosComponent implements OnInit {
+  displayedColumns: string[] = ['usuario', 'nombre', 'apellido', 'sexo'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
